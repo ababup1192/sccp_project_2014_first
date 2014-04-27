f2851e9114f089753cd679f054cb3bcaa7cbb7f9
ababup1192 <ababup1192@gmail.com>
2014-04-28 01:07:30 +0900

    add document
    add ruby document for beginner.

--- grade_calculator/rb_grade_calc/grade_calculator2.rb
+++ grade_calculator/rb_grade_calc/grade_calculator2.rb
@@ -4,7 +4,9 @@ table = CSV.table('grade.csv')
 grade_list  = table.headers.map do |e|
 	table[e].inject(0){|sum, elm| sum + elm}
 end
+
 sum = grade_list.inject(0){|sum, elm| sum + elm}
 avg = sum / (table.size * table.headers.size).to_f
+
 puts "sum = #{sum}"
 puts "sum = #{avg}"
