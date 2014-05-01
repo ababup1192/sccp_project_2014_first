227b25f21e102318e92a1549c4bc0b1ba5580e18
ababup1192 <ababup1192@gmail.com>
2014-05-01 17:36:34 +0900

    add ruby simple program

---
+++ ruby_simple_samples/array.rb
@@ -0,0 +1,4 @@
+cattle = "google"
+p container = [cattle, cattle]
+cattle[2] = 'p'
+p container
---
+++ ruby_simple_samples/array2.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+a = 1
+b = "str"
+c = [a, b, 3, "文字列"]
+p c
+d = [a, c, [1, 2, 3]]
+p d
